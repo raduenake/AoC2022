41541d32a764d1897ae02ba46f5ef19136031bbf
Radu E <radu.enache@nagarro.com>
2022-12-05 17:57:41 +0200

    03, 04

---
+++ 03/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Immutable;
+
+var file = System.IO.File.OpenText("input.txt");
+var input = file.ReadToEnd()
+    .Split("\r\n")
+    .Where(l => !string.IsNullOrEmpty(l))
+    .ToImmutableArray();
+
+var common = input.Select(l =>
+{
+    var lChar = l.Select(c => c);
+    var left = lChar.Take(lChar.Count() / 2).ToImmutableArray();
+    var right = lChar.Skip(lChar.Count() / 2).ToImmutableArray();
+    return left.First(c => right.Any(rc => rc == c));
+}).ToImmutableArray();
+
+var p1 = common.Sum(c => Char.IsLower(c) switch { true => 1 + (c - 'a'), false => 27 + (c - 'A') });
+Console.WriteLine($"Part 1: {p1}");
+
+var chunkedCommon = input.Chunk(3).Select(l => l[0].First(c => l[1].Any(rc => rc == c && l[2].Any(rcc => rcc == c))))
+    .ToImmutableArray();
+var p2 = chunkedCommon.Sum(c => Char.IsLower(c) switch { true => 1 + (c - 'a'), false => 27 + (c - 'A') });
+
+Console.WriteLine($"Part 2: {p2}");
---
+++ 04/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Immutable;
+
+var file = System.IO.File.OpenText("input.txt");
+var input = file.ReadToEnd()
+    .Split("\r\n")
+    .Where(l => !string.IsNullOrEmpty(l))
+    .ToImmutableArray();
+
+var ranges = input.Select(l =>
+{
+    var s = l.Split(',');
+    var s1 = s[0].Split('-').Select(c => int.Parse(c)).ToArray();
+    var s2 = s[1].Split('-').Select(c => int.Parse(c)).ToArray();
+    return (left: (s1[0], s1[1]), right: (s2[0], s2[1]));
+}).ToImmutableArray();
+
+var x = ranges.Sum(r =>
+    ((r.left.Item1 <= r.right.Item1 && r.left.Item2 >= r.right.Item2) ||
+    (r.right.Item1 <= r.left.Item1 && r.right.Item2 >= r.left.Item2))
+        ? 1 : 0
+);
+
+Console.WriteLine($"Part 1: {x}");
+
+var y = ranges.Sum(r =>
+{
+    var one = Enumerable.Range(r.left.Item1, (r.left.Item2 - r.left.Item1) + 1).ToArray();
+    var two = Enumerable.Range(r.right.Item1, (r.right.Item2 - r.right.Item1) + 1).ToArray();
+    return one.Intersect(two).Any() ? 1 : 0;
+});
+
+Console.WriteLine($"Part 2: {y}");
