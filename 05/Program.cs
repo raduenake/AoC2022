621a21bd03531e203af64e54568cb06ccb53c5c6
Radu E <radu.enache@nagarro.com>
2022-12-06 10:17:56 +0200

    05, 06

---
+++ 05/Program.cs
@@ -0,0 +1,74 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+
+var file = System.IO.File.OpenText("input.txt");
+var input = file.ReadToEnd()
+    .Split("\r\n")
+    .Where(l => !string.IsNullOrEmpty(l))
+    .ToImmutableArray();
+
+var crates = input.Take(8).Reverse().ToImmutableArray();
+var startStacks = Enumerable.Range(0, 9).Select(i => new List<string>()).ToImmutableArray();
+
+foreach (var crateLine in crates)
+{
+    var stack = 0;
+    for (int i = 2; i < crateLine.Length; i += 3)
+    {
+        var crate = string.Join("", crateLine.Skip(i - (3 - 1)).Take(3))
+            .Replace("[", "")
+            .Replace("]", "");
+
+        if (!string.IsNullOrWhiteSpace(crate))
+        {
+            startStacks[stack].Add(crate);
+        }
+        // increment stack
+        stack++;
+        // skip empty space
+        i++;
+    }
+}
+
+Func<string, (int, int, int)> parseInstruction = (instr) =>
+{
+    var rx = new Regex(@"^move (.*) from (.*) to (.*)$");
+    var match = rx.Match(instr);
+
+    var amount = int.Parse(match.Groups[1].Value);
+    var source = int.Parse(match.Groups[2].Value) - 1;
+    var dest = int.Parse(match.Groups[3].Value) - 1;
+
+    return (amount, source, dest);
+};
+
+var stacks1 = startStacks.Select(s => new Stack<string>(s)).ToImmutableArray();
+foreach (var instr in input.Skip(9).Where(l => !string.IsNullOrWhiteSpace(l)))
+{
+    var pInstr = parseInstruction(instr);
+    for (int i = 0; i < pInstr.Item1; i++)
+    {
+        stacks1[pInstr.Item3].Push(stacks1[pInstr.Item2].Pop());
+    }
+}
+Console.WriteLine($"Part1 : {string.Join("", stacks1.Select(s => s.Peek()))}");
+
+var stacks2 = startStacks.Select(s => new Stack<string>(s)).ToImmutableArray();
+foreach (var instr in input.Skip(9).Where(l => !string.IsNullOrWhiteSpace(l)))
+{
+    var pInst = parseInstruction(instr);
+
+    var cratesPopped = new List<string>();
+    for (int i = 0; i < pInst.Item1; i++)
+    {
+        cratesPopped.Add(stacks2[pInst.Item2].Pop());
+    }
+
+    cratesPopped.Reverse();
+    foreach (var el in cratesPopped)
+    {
+        stacks2[pInst.Item3].Push(el);
+    }
+}
+Console.WriteLine($"Part1 : {string.Join("", stacks2.Select(s => s.Peek()))}");
---
+++ 06/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Immutable;
+
+var file = System.IO.File.OpenText("input.txt");
+var input = file.ReadToEnd()
+    .Split("\r\n")
+    .Where(l => !string.IsNullOrEmpty(l))
+    .ToImmutableArray();
+
+// the message line
+var line = input.First();
+
+Func<string, int, int, bool> solution = (line, start, length) =>
+{
+    var chs = line.Skip(start - (length - 1)).Take(length);
+    return !chs.Where((c, i) => chs.Where((c1, i1) => i1 != i && c1 == c).Any()).Any();
+};
+
+var idx = 3;
+for (int i = idx; i < line.Length; i++)
+{
+    var done = solution(line, i, 4);
+    if (done)
+    {
+        idx = i;
+        break;
+    }
+}
+Console.WriteLine($"Part 1: {idx + 1}");
+
+// 13 chars 
+idx = 13;
+for (int i = idx; i < line.Length; i++)
+{
+    var done = solution(line, i, 14);
+    if (done)
+    {
+        idx = i;
+        break;
+    }
+}
+
+Console.WriteLine($"Part 2: {idx + 1}");
